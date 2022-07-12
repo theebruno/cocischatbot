eede98150462c3ea07875f2f5504113fbae87c22
theebruno <51233837+theebruno@users.noreply.github.com>
2022-07-12 12:17:22 +0300

    adding app

--- app/chatbot/lib/main.dart
+++ app/chatbot/lib/main.dart
@@ -1,115 +1,110 @@
+import 'dart:convert';
+import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
+import 'package:http/http.dart' as http;
+void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+// This widget is the root of our application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'COCIS BOT',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+primarySwatch: Colors.greenAccent,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Hello World'),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  MyHomePage({Key key, this.title}) : super(key: key);
+final String title;
+@override
+  _MyHomePageState createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+final GlobalKey<AnimatedListState> _listKey = GlobalKey();
+  List<String> _data = [];
+  static const String BOT_URL = "https://supercodebot.herokuapp.com"; // replace with server address
+  TextEditingController _queryController = TextEditingController();
+@override
+  Widget build(BuildContext context) {
+return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Flutter & Python"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          AnimatedList(
+            // key to call remove and insert from anywhere
+            key: _listKey,
+            initialItemCount: _data.length,
+            itemBuilder: (BuildContext context, int index, Animation animation){
+              return _buildItem(_data[index], animation, index);
+            }
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: TextField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.message, color: Colors.greenAccent,),
+                hintText: "Hello",
+              ),
+              controller: _queryController,
+              textInputAction: TextInputAction.send,
+              onSubmitted: (msg){
+this._getResponse();
+              },
+),
+          )
+        ],
+      )
+      ,
+    );
+  }
+http.Client _getClient(){
+    return http.Client();
+  }
+//this method receives the json response from our api
+void _getResponse(){
+    if (_queryController.text.length>0){
+      this._insertSingleItem(_queryController.text);
+      var client = _getClient();
+      try{
+        client.post(BOT_URL, body: {"query" : _queryController.text},)
+        ..then((response){
+          Map<String, dynamic> data = jsonDecode(response.body);
+          _insertSingleItem(data['response']+"<bot>");
+});
+      }catch(e){
+        print("Failed -> $e");
+      }finally{
+        client.close();
+        _queryController.clear();
+      }
+    }
+  }
+//this method  inserts the add the reponse to the list
+void _insertSingleItem(String message){
+_data.add(message); 
+    _listKey.currentState.insertItem(_data.length-1);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+// this widget builds the chat replies and messages using bubble from pub get
+Widget _buildItem(String item, Animation animation,int index){
+    bool mine = item.endsWith("<bot>");
+    return SizeTransition(
+      sizeFactor: animation,
+      child: Padding(padding: EdgeInsets.only(top: 10),
+      child: Container(
+        alignment: mine ?  Alignment.topLeft : Alignment.topRight,
+child : Bubble(
+        child: Text(item.replaceAll("<bot>", "")),
+        color: mine ? Colors.blue : Colors.indigo,
+        padding: BubbleEdges.all(10),
+)),
+    )
+);
   }
 }
