94620fd795aeea348cff94ffb18c4c8aac6eedd3
theebruno <51233837+theebruno@users.noreply.github.com>
2022-07-12 10:36:16 +0300

    testing heroku server

--- bot/app.py
+++ bot/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__);
 @app.route("/bot", methods=["POST"])
 def response():
     query = dict(request.form)['query']
-    res = query + " " + time.ctime()
+    res = query + " " + time.ctime()+"is the time"
     return jsonify({"response" : res})
 if __name__=="__main__":
     app.run(host="0.0.0.0",)
