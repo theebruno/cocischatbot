5f00d44ceeda6d4ab667b52862471ca9493bc3f1
theebruno <51233837+theebruno@users.noreply.github.com>
2022-07-13 16:33:08 +0300

    depo

--- app.py
+++ app.py
@@ -20,9 +20,50 @@ for file in os.listdir('data/'):
 @app.route("/")
 def hello():
     return render_template('chat.html')
-
-@app.route("/ask/", methods=['GET'])
+#for web
+@app.route("/ask", methods=['POST'])
 def ask():
+
+    message = str(request.form['messageText'])
+
+    bot_response = bot.get_response(message)
+
+    while True:
+
+        if bot_response.confidence > 0.1:
+
+            bot_response = str(bot_response)      
+            print(bot_response)
+            return jsonify({'status':'OK','answer':bot_response})
+ 
+        elif message == ("bye"):
+
+            bot_response='Hope to see you soon'
+
+            print(bot_response)
+            return jsonify({'status':'OK','answer':bot_response})
+
+            break
+
+        else:
+        
+            try:
+                url  = "https://en.wikipedia.org/wiki/"+ message
+                page = get(url).text
+                soup = BeautifulSoup(page,"html.parser")
+                p    = soup.find_all("p")
+                return jsonify({'status':'OK','answer':p[1].text})
+
+            except IndexError as error:
+
+                bot_response = 'Sorry i have no idea about that.'
+            
+                print(bot_response)
+                return jsonify({'status':'OK','answer':bot_response})
+
+#for mobile app
+@app.route("/tell", methods=['GET'])
+def tell():
 
     message = request.args.get("query", None)
 
