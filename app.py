50ac4adc054aa42ccf61242798d4779f7d45a707
theebruno <51233837+theebruno@users.noreply.github.com>
2022-07-14 08:42:41 +0300

    depo

--- app.py
+++ app.py
@@ -1,24 +1,7 @@
-from chatbot import chatbot
-from flask import Flask, render_template, request,jsonify
-import mysql.connector
-
+from flask import Flask, request, jsonify
 app = Flask(__name__)
-app.static_folder = 'static'
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/get")
-def get_bot_response():
-    userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route('/getmsg', methods=['GET'])
+@app.route('/getmsg/', methods=['GET'])
 def respond():
     # Retrieve the name from url parameter
     name = request.args.get("query", None)
@@ -40,31 +23,29 @@ def respond():
 
     # Return the response in json format
     # return jsonify(response)
-    return jsonify({"response" : str(chatbot.get_response(name))})    
+    return jsonify({"response" : det})
 
-@app.route('/result',methods=['POST','GET'])
-def result():
-    mydb=mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="chatbot"
-    )
-    mycursor=mydb.cursor()
-    if request.method=='POST':
-        signup=request.form
-        username = signup['username']
-        password = signup['password']
-        mycursor.excute("select * from reg where Username='"+username+"' and Password='"+password+"'")
-        r=mycursor.fetchall()
-        count=mycursor.rowcount
-        if count==1:
-            return render_template("index.html")
-        else:
-            return render_template("login.html")
-        
-    mydb.commit()
-    mycursor.close()
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
 
-if __name__ == "__main__":
-    app.run() 
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
