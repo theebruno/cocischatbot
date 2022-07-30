6b357e74d4bf7aaaeb4ae554b32e5f007de513a5
LwangaConrad <lwangaconradarthur@gmail.com>
2022-07-30 15:12:19 +0300

    minor fixes
    Spelling and naming fixes

--- chatty/chatbot.py
+++ chatty/chatbot.py
@@ -1,7 +1,7 @@
 import random
 import json 
 import pickle
-import numpy as numpy
+import numpy as np
 import nltk
 
 from nltk.stem import WordNetLemmatizer
@@ -10,8 +10,8 @@ from tensorflow.keras.models import load_model
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
 
-words = pickle.load(open('words.pkl', rb))
-classes = pickle.load(open('classes.pkl', rb))
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('chatbotmodel.h5')
 
 def clean_up_sentence(sentence):
@@ -33,12 +33,12 @@ def predict_class(sentence):
 	bow = bag_of_words(sentence)
 	res = model.predict(np.array([bow]))[0]
 	ERROR_THRESHOLD = 0.25
-	result = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+	results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
 
 	results.sort(key=lambda x: x[1], reverse=True)
 	return_list = []
 	for r in results:
-		return_list.append({intent: classes[r[0]], 'probability': str(r[1])})
+		return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
 	return return_list
 
 def get_response(intents_list, intents_json):
@@ -46,7 +46,7 @@ def get_response(intents_list, intents_json):
 	list_of_intents = intents_json['intents']
 	for i in list_of_intents:
 		if i['tag'] == tag:
-			result = random.cjoice(i['reposnses'])
+			result = random.choice(i['responses'])
 			break
 	return result
 
