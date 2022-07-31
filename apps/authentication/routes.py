c6515db877b50f3f68d02a65a9fdbaa2d103ebf7
theebruno <51233837+theebruno@users.noreply.github.com>
2022-07-31 19:34:25 +0300

    deployment

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -27,7 +27,6 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
 
-
 @blueprint.route('/')
 def route_default():
     return redirect(url_for('authentication_blueprint.login'))
@@ -128,7 +127,54 @@ def not_found_error(error):
 @blueprint.errorhandler(500)
 def internal_error(error):
     return render_template('home/page-500.html'), 500
+
+
+
 #addding ai module
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbotmodel.h5')
+
+def clean_up_sentence(sentence):
+	sentence_words = nltk.word_tokenize(sentence)
+	sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+	return sentence_words
+
+
+def bag_of_words(sentence):
+	sentence_words = clean_up_sentence(sentence)
+	bag = [0] * len(words)
+	for w in sentence_words:
+		for i, word in enumerate(words):
+			if word == w:
+				bag[i] = 1
+	return np.array(bag)
+
+def predict_class(sentence):
+	bow = bag_of_words(sentence)
+	res = model.predict(np.array([bow]))[0]
+	ERROR_THRESHOLD = 0.25
+	results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+	results.sort(key=lambda x: x[1], reverse=True)
+	return_list = []
+	for r in results:
+		return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+	return return_list
+
+def get_response(intents_list, intents_json):
+	tag= intents_list[0]['intent']
+	list_of_intents = intents_json['intents']
+	for i in list_of_intents:
+		if i['tag'] == tag:
+			result = random.choice(i['responses'])
+			break
+	return result
+
+print("Chatbot is now active")
 
 @blueprint.route('/getmsg/', methods=['GET'])
 def respond():
@@ -137,7 +183,10 @@ def respond():
 
     
     while True:
-            input_text = name
+        
+        message = name
+	    ints = predict_class(message)
+	    res = get_response(ints, intents)
             
-            return jsonify({"response" : name})
+        return jsonify({"response" : name})
 
