23561ac69f484954c0c861109f5819d83bb89755
theebruno <51233837+theebruno@users.noreply.github.com>
2022-07-31 15:17:07 +0300

    deployment

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -3,22 +3,12 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from flask import render_template, redirect, jsonify,request, url_for
+from flask import render_template, redirect, request, url_for
 from flask_login import (
     current_user,
     login_user,
     logout_user
 )
-#for the ai
-import random
-import json 
-import pickle
-import numpy as np
-import nltk
-
-from nltk.stem import WordNetLemmatizer
-from tensorflow.keras.models import load_model
-#
 
 from apps import db, login_manager
 from apps.authentication import blueprint
@@ -31,67 +21,7 @@ from apps.authentication.util import verify_pass
 @blueprint.route('/')
 def route_default():
     return redirect(url_for('authentication_blueprint.login'))
-# adding the ai module    
 
-lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('intents.json').read())
-
-words = pickle.load(open('words.pkl', 'rb'))
-classes = pickle.load(open('classes.pkl', 'rb'))
-model = load_model('chatbotmodel.h5')
-
-def clean_up_sentence(sentence):
-  sentence_words = nltk.word_tokenize(sentence)
-  sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
-  return sentence_words
-
-
-def bag_of_words(sentence):
-  sentence_words = clean_up_sentence(sentence)
-  bag = [0] * len(words)
-  for w in sentence_words:
-    for i, word in enumerate(words):
-      if word == w:
-        bag[i] = 1
-  return np.array(bag)
-
-def predict_class(sentence):
-  bow = bag_of_words(sentence)
-  res = model.predict(np.array([bow]))[0]
-  ERROR_THRESHOLD = 0.25
-  results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-
-  results.sort(key=lambda x: x[1], reverse=True)
-  return_list = []
-  for r in results:
-    return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
-  return return_list
-
-def get_response(intents_list, intents_json):
-  tag= intents_list[0]['intent']
-  list_of_intents = intents_json['intents']
-  for i in list_of_intents:
-    if i['tag'] == tag:
-      result = random.choice(i['responses'])
-      break
-  return result
-
-print("Chatbot is now active")
-
-
-@blueprint.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    message = request.args.get("query", None)
-
-    
-    while True:
-      
-      ints = predict_class(message)
-      res = get_response(ints, intents)
-      return jsonify({"response" : res})
-
-###############
 
 # Login & Registration
 
