988776a45afd370d31b493140d9c31eed4773fc4
theebruno <51233837+theebruno@users.noreply.github.com>
2022-07-31 13:07:50 +0300

    deployment

---
+++ apps/authentication/routes.py
@@ -0,0 +1,190 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from flask import render_template, redirect, jsonify,request, url_for
+from flask_login import (
+    current_user,
+    login_user,
+    logout_user
+)
+#for the ai
+import random
+import json 
+import pickle
+import numpy as np
+import nltk
+
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+#
+
+from apps import db, login_manager
+from apps.authentication import blueprint
+from apps.authentication.forms import LoginForm, CreateAccountForm
+from apps.authentication.models import Users
+
+from apps.authentication.util import verify_pass
+
+
+@blueprint.route('/')
+def route_default():
+    return redirect(url_for('authentication_blueprint.login'))
+# adding the ai module    
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbotmodel.h5')
+
+def clean_up_sentence(sentence):
+  sentence_words = nltk.word_tokenize(sentence)
+  sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+  return sentence_words
+
+
+def bag_of_words(sentence):
+  sentence_words = clean_up_sentence(sentence)
+  bag = [0] * len(words)
+  for w in sentence_words:
+    for i, word in enumerate(words):
+      if word == w:
+        bag[i] = 1
+  return np.array(bag)
+
+def predict_class(sentence):
+  bow = bag_of_words(sentence)
+  res = model.predict(np.array([bow]))[0]
+  ERROR_THRESHOLD = 0.25
+  results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+  results.sort(key=lambda x: x[1], reverse=True)
+  return_list = []
+  for r in results:
+    return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+  return return_list
+
+def get_response(intents_list, intents_json):
+  tag= intents_list[0]['intent']
+  list_of_intents = intents_json['intents']
+  for i in list_of_intents:
+    if i['tag'] == tag:
+      result = random.choice(i['responses'])
+      break
+  return result
+
+print("Chatbot is now active")
+
+
+@blueprint.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    message = request.args.get("query", None)
+
+    
+    while True:
+      
+      ints = predict_class(message)
+      res = get_response(ints, intents)
+      return jsonify({"response" : res})
+
+###############
+
+# Login & Registration
+
+@blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm(request.form)
+    if 'login' in request.form:
+
+        # read form data
+        username = request.form['username']
+        password = request.form['password']
+
+        # Locate user
+        user = Users.query.filter_by(username=username).first()
+
+        # Check the password
+        if user and verify_pass(password, user.password):
+
+            login_user(user)
+            return redirect(url_for('authentication_blueprint.route_default'))
+
+        # Something (user or pass) is not ok
+        return render_template('accounts/login.html',
+                               msg='Wrong user or password',
+                               form=login_form)
+
+    if not current_user.is_authenticated:
+        return render_template('accounts/login.html',
+                               form=login_form)
+    return redirect(url_for('home_blueprint.index'))
+
+
+@blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    create_account_form = CreateAccountForm(request.form)
+    if 'register' in request.form:
+
+        username = request.form['username']
+        email = request.form['email']
+
+        # Check usename exists
+        user = Users.query.filter_by(username=username).first()
+        if user:
+            return render_template('accounts/register.html',
+                                   msg='Username already registered',
+                                   success=False,
+                                   form=create_account_form)
+
+        # Check email exists
+        user = Users.query.filter_by(email=email).first()
+        if user:
+            return render_template('accounts/register.html',
+                                   msg='Email already registered',
+                                   success=False,
+                                   form=create_account_form)
+
+        # else we can create the user
+        user = Users(**request.form)
+        db.session.add(user)
+        db.session.commit()
+
+        return render_template('accounts/register.html',
+                               msg='User created please <a href="/login">login</a>',
+                               success=True,
+                               form=create_account_form)
+
+    else:
+        return render_template('accounts/register.html', form=create_account_form)
+
+
+@blueprint.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('authentication_blueprint.login'))
+
+
+# Errors
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return render_template('home/page-403.html'), 403
+
+
+@blueprint.errorhandler(403)
+def access_forbidden(error):
+    return render_template('home/page-403.html'), 403
+
+
+@blueprint.errorhandler(404)
+def not_found_error(error):
+    return render_template('home/page-404.html'), 404
+
+
+@blueprint.errorhandler(500)
+def internal_error(error):
+    return render_template('home/page-500.html'), 500
