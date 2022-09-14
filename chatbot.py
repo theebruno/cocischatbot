91f06a1d4f0897a1e74fb114e9c4fb1d619001fa
LwangaConrad <lwangaconradarthur@gmail.com>
2022-09-14 20:28:49 +0300

    fixes

--- chatbot.py
+++ chatbot.py
@@ -12,22 +12,28 @@ nltk.download('punkt')
 from nltk.stem import WordNetLemmatizer
 #for a sort of combined tokenization and stemming
 from tensorflow.keras.models import load_model
+#for loading the model that we created in training.py
 
-lemmatizer = WordNetLemmatizer()
 #create the lemmatizer to be used
-intents = json.loads(open('intents.json').read())
-#loading the json file for the intents
+lemmatizer = WordNetLemmatizer()
 
+#loading the json file for the intents
+intents = json.loads(open('intents.json').read())
+
+#load the pickle files in reading binary mode
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
+
+#loading the model
 model = load_model('chatbotmodel.h5')
 
+#function for lemmatizing and tokenizing the sentence
 def clean_up_sentence(sentence):
 	sentence_words = nltk.word_tokenize(sentence)
 	sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
 	return sentence_words
 
-
+#function for conversion of a sentence into a list that denotes the occurence of a word in the sentence using 0s and 1s
 def bag_of_words(sentence):
 	sentence_words = clean_up_sentence(sentence)
 	bag = [0] * len(words)
@@ -37,29 +43,45 @@ def bag_of_words(sentence):
 				bag[i] = 1
 	return np.array(bag)
 
+#function for predicting the class a sentence falls under 
 def predict_class(sentence):
 	bow = bag_of_words(sentence)
 	res = model.predict(np.array([bow]))[0]
+	res_index = np.argmax(res)
+	global var
+	var = 0
+	if res[res_index] < 0.6:
+		var = 1
+	#specifies the level of uncertainity that is acceptable for our model prediction
 	ERROR_THRESHOLD = 0.25
 	results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
 
+	#sort in descending order with highest probability first
 	results.sort(key=lambda x: x[1], reverse=True)
 	return_list = []
 	for r in results:
 		return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
 	return return_list
 
+#function for gettig the response based off the class predicted by the model
 def get_response(intents_list, intents_json):
-	tag= intents_list[0]['intent']
-	list_of_intents = intents_json['intents']
-	for i in list_of_intents:
-		if i['tag'] == tag:
-			result = random.choice(i['responses'])
-			break
-	return result
+	try:
+		tag= intents_list[0]['intent']
+		list_of_intents = intents_json['intents']
+		for i in list_of_intents:
+			if var == 1:
+				result = "I have not quite understood your question please rephrase it for me"
+			elif i['tag'] == tag:
+				result = random.choice(i['responses'])
+				break
+		return result
+	except:
+		result = "I am having a hard time understanding your question please rephrase it for me"
+		return result
 
 print("Chatbot is now active")
 
+#loop for running the chatbot
 while True:
 	message = input("")
 	if message == "quit":
